be7d6c7a1afca8c1def671402bffc83ae7eca825
phucpham <phamhuuphuc4049@gmail.com>
2025-02-05 11:16:16 +0700

    feat(cors): adding cors config to fetchAPI

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: 'http://localhost:3000' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -14,6 +15,6 @@ async function bootstrap() {
       },
     }),
   );
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
