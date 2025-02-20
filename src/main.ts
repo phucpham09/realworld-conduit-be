9e7359c9f91722a0f7bf694247e4895e82ff6060
phucpham <phamhuuphuc4049@gmail.com>
2025-02-20 22:40:42 +0700

    fix(cors): changing the origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: ['https://realworld-conduit-fe.vercel.app'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
