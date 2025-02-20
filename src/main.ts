85297f8738a7610ef7f7c4ab5697da85f54a2ddb
phucpham <phamhuuphuc4049@gmail.com>
2025-02-20 22:18:49 +0700

    fix(cors): fixing the frontend address to connect

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: '*' });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
